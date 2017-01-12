8daf4579ece780da6b77131b81d57178f7ef1ecd
DavidSV2017 <dsv5@txstate.edu>
2017-01-11 21:03:19 -0600

    Updated the algorithm for MC2

--- MCU2CodeArduinoUno/MCU2CodeArduinoUno.ino
+++ MCU2CodeArduinoUno/MCU2CodeArduinoUno.ino
@@ -11,7 +11,9 @@
 // the Mega board. 
 
 #include "NewPing.h"
-#include "SPI.h"
+#include "EMFSensor.h"
+#include "CapacitiveSensor.h"
+#include <SPI.h>
 
 // This declaration and initialization is for the Slave Select Pin. The
 // SPI library will automatically set the other SPI Pins.
@@ -25,22 +27,28 @@ int SS = 10;
 // This constructor is for the UltraSonic Distance Sensor.
 NewPing sonar(TRIGGER_PIN = 8, ECHO_PIN = 7, MAX_DISTANCE = 200);
 
-// This constructor is for the SPI library found online.
-SPIClass comm;
-
 // This function will return a True or False bool value depending on
 // if an obstacle is detected on the field.
 bool read_Obstacle();
+
+// This function determines the configuration of the extension cable.
+int cableConfig(float, float, float, float);
+
+// These four constructors are for the four EMF Sensors. I labeled them 
+// as N=North, S=South, W=West, E=East.
+EMFSensor sensorN;
+EMFSensor sensorS;
+EMFSensor sensorW;
+EMFSensor sensorE;
+
+// This is the constructor for the capacitive sensor.
+CapacitiveSensor sensorC;
 
 // This void function is used to setup important features
 // of the Arduino microprocessor board.
 void setup() {
   
   Serial.begin(115200);
-
-  comm.begin();
-
-  
 
   /*
   // This code is related to the SPI communication code.
@@ -56,11 +64,76 @@ void setup() {
 
 // This void function acts just like the main function.
 void loop() {
-  
+  // This variable will be used to store whether or not there is an obstacle in
+  // the upcoming one foot square section.
+  bool scan4Obstacle;
 
+  // These four bool variables will be used by the corresponding
+  // EMF sensor.
+  bool voltDectN;
+  bool voltDectE;
+  bool voltDectS;
+  bool voltDectW;
+
+  // This variable will be used to store whether or not there is insulation 
+  // below the layer of glass.
+  bool insulationDect;
+
+  // This variable will be used to communicate to the MCU that drives the motors.
+  int currentCableConfig = 0;
+  
+  // Use the read_Obstacle function to determine if there is an
+  // obstacle.
+  scan4Obstacle = read_Obstacle;
+
+  if(scan4Obstacle == false) {
+    voltDectN = false;
+    voltDectE = false;
+    voltDectS = false;
+    voltDectW = false;
+    
+    // This code uses all four EMF sensors to scan for a voltage.
+    sensorN.getVoltage();
+    sensorE.getVoltage();
+    sensorS.getVoltage();
+    sensorW.getVoltage();
+
+    voltDectN = sensorN.checkRange();
+    voltDectE = sensorE.checkRange();
+    voltDectS = sensorS.checkRange();
+    voltDectW = sensorW.checkRange();
+
+    // This if/statement determines whether or not any of the four EMF sensors detects a
+    // voltage. 
+    if(voltDectN == true || voltDectE == true || voltDectS == true || voltDectW == true) {
+      // I (David) need to include the code for lighting the LED array at this point in
+      // code. Look at Jorge's implementation code and add it here.
+
+      // The following code will determine if the extension cable is running in a straight 
+      // line or if there is a bend at this particular square.
+      
+      
+    }
+    else {
+      //  This code will determine whether or not there is insulation present at the current
+      // location of the robot. 
+      
+
+      // This if statement will determine whether or not there is insulation present below 
+      // the glass layer.
+      if( ) {
+        
+      }
+    }
+  }
+  else
+  {
+    // This else statement will send a variable to the MCU that drives the motors. The purpose of 
+    // variable is to adjust the path of the robot to avoid the obstacle.
+  }
 }
 
-
+// This function is used to determine if there is an obstacle on the i
 bool read_Obstacle() {
   // This variable stores the values calculated by the UltraSonic
   // hardware. In addition, the variable is initialized using the 
@@ -84,5 +157,52 @@ bool read_Obstacle() {
   else {
     return false;
   }
+}
+
+// This function will determine the configuration of the extension cord. There are 
+// six different configurations. Each configuration will be identified by an integer
+// variable (int values 1 - 6), look at the paper documentation for more information. This 
+// function will return the int value of the configuration.
+int cableConfig(float vN, float vE, float vS, float vW) {
+  // The 'expectedV float variable will be determined during testing of the EMF sensors. This
+  // value is an expected voltage but the EMF sensors will rarely if ever return this value. The
+  // programmer must determine a percent range of acceptable voltage values.  
+  const float expectedV = 5.0;
+
+  const float minRangeV = 0.95 * expectV;
+  const float maxRangeV = 1.05 * expectV;
+
+  int configCable = 0;
+
+  // The following six if statements determine the configuration of the extension cable 
+  // at the current 1 square foot section. Config 1 is the cable running west to east. Config 2 
+  // is the cable running north to south. Config 3 is the cable running north to east. Config 4
+  // is the cable running south to east. Config 5 is the extension cable running west to south. 
+  // Config 6 is the extension cable running west to north.
+  if((minRangeV <= vW && vW <= maxRangeV) && (minRangeV <= vE && vE <= maxRangeV)) {
+    configCable = 1; 
+  }
+
+  if((minRangeV <= vN && vN <= maxRangeV) && (minRangeV <= vS && vS <= maxRangeV)) {
+    configCable = 2; 
+  }
+
+  if((minRangeV <= vN && vN <= maxRangeV) && (minRangeV <= vE && vE <= maxRangeV)) {
+    configCable = 3; 
+  }
+
+    if((minRangeV <= vE && vE <= maxRangeV) && (minRangeV <= vS && vS <= maxRangeV)) {
+    configCable = 4; 
+  }
+
+    if((minRangeV <= vW && vW <= maxRangeV) && (minRangeV <= vS && vS <= maxRangeV)) {
+    configCable = 5; 
+  }
+
+    if((minRangeV <= vW && vW <= maxRangeV) && (minRangeV <= vN && vN <= maxRangeV)) {
+    configCable = 6; 
+  }
+
+  return configCable;
 }
 
