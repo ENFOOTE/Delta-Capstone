291b58e60de433974eb9ff697faa57cc90a42527
DavidSV2017 <dsv5@txstate.edu>
2017-02-22 20:09:12 -0600

    Add files via upload

--- MCU2CodeArduinoUno/MCU2CodeArduinoUno.ino
+++ MCU2CodeArduinoUno/MCU2CodeArduinoUno.ino
@@ -1,10 +1,15 @@
-// EE4390 EE Design I
+// EE4390 EE Design I and EE4391 EE Design II
 // Date created: December 01, 2016 
-// Author One: David S Vasquez
-// Author Two: Eric Foote
-// Author Three: Jorge Villalobos
-// Libraries: "NewPing.h" is used for the UltraSonic Distance Sensor, "SPI.h" 
-// is used for the SPI communication between the Arduino boards.
+// Programmer One: David S Vasquez
+// Programmer Two: Geovanni Hernandez
+// Programmer Three: Eric Foote
+// Programmer Four: Jorge Villalobos
+// Libraries: "NewPing.h" is used for the UltraSonic Distance Sensor, <SPI.h> 
+// is used for the SPI communication between the Arduino boards. "EMFSensor.h" 
+// is used for the custom made four EMF sensors, these sensors capture and amplify
+// a 60 Hz signal. The "CapacitiveSensor.h" is used for capturing the unique 
+// voltage of the insulation or dead-end tunnel. The "ArrayLED.h" library is 
+// used to light individual LEDs on the LED matrix.
 // Information: This is the master code program for the Arduino Uno
 // board (second board MC2). This program will be used by all the 
 // hardware connected to this board. This program communicate with 
@@ -18,7 +23,7 @@
 
 // This declaration and initialization is for the Slave Select Pin. The
 // SPI library will automatically set the other SPI Pins.
-int SS = 10;
+// int SS = 10;
 
 // The variable called 'US_ROUNDTRIP_MM' was created specifically so 
 // the sensor would return distance values in millimeters. Normally, 
@@ -47,10 +52,10 @@ CapacitiveSensor sensorC;
 
 // This int variable will be used to communicate an important value to the
 // master Arduino board. Value 1 = extension cable config West-East, value 2 = extension
-// cable config North-South, value 3 = extension cable config North-East, value 4 = extension
-// cable config South-East, value 5 = extension cable config West-South, value 6 = extension 
-// cable config North-West, value 6 = forward go, value 8 = stop motion, value 9 = obstacle 
-// detected, value 0 = error.
+// cable config North-South, value 3 = extension cable config North-East, value 4 = 
+// extension cable config South-East, value 5 = extension cable config West-South, value 
+// 6 = extension cable config North-West, value 6 = forward go, value 8 = stop motion, 
+// value 9 = obstacle detected, value 0 = error.
 int legendValueSPI;
 
 // These string variables will be used for the LED features. The 'strLoc' is the current 
@@ -59,97 +64,101 @@ int legendValueSPI;
 string strLoc;
 string colorLEDcurr;
 
+// This is the class object instance of the LED using Jorge's LED code.
+ArrayLED mapObject;
+
 // This void function is used to setup important features
 // of the Arduino microprocessor board.
 void setup() {
-  
   Serial.begin(115200);
 }
 
 // This void function acts just like the main function.
 void loop() {
-  // I (David) needs to write code that uses SPI to receive the current location
-  // per the master Arduino board.
-
-
-  strLoc = 
-
-  
-  // This variable will be used to store whether or not there is an obstacle in
-  // the upcoming one foot square section.
-  bool scan4Obstacle;
-
-  // These four bool variables will be used by the corresponding
-  // EMF sensor.
-  bool voltDectN;
-  bool voltDectE;
-  bool voltDectS;
-  bool voltDectW;
-
-  // This bool variable will be used to store whether or not there is insulation 
-  // below the layer of glass.
-  bool insulationDect;
-
-  // This variable will be used to communicate to the MCU that drives the motors.
-  int currentCableConfig = 0;
-  
-  // Use the read_Obstacle function to determine if there is an
-  // obstacle.
-  scan4Obstacle = read_Obstacle;
-
-  if(scan4Obstacle == false) {
-    voltDectN = false;
-    voltDectE = false;
-    voltDectS = false;
-    voltDectW = false;
+  // This if statement will execute the sensor algorithm.
+  if(/*argument will become true because of a SPI command*/) {
     
-    // This code uses all four EMF sensors to scan for a voltage.
-    sensorN.getVoltage();
-    sensorE.getVoltage();
-    sensorS.getVoltage();
-    sensorW.getVoltage();
+    // I (David) needs to write code that uses SPI to receive the current location
+    // per the master Arduino board.
+    strLoc = 
 
-    voltDectN = sensorN.checkRange();
-    voltDectE = sensorE.checkRange();
-    voltDectS = sensorS.checkRange();
-    voltDectW = sensorW.checkRange();
+  
+    // This variable will be used to store whether or not there is an obstacle in
+    // the upcoming one foot square section.
+    bool scan4Obstacle;
 
-    // This if/statement determines whether or not any of the four EMF sensors detects a
-    // voltage. 
-    if(voltDectN == true || voltDectE == true || voltDectS == true || voltDectW == true) {
-      // I (David) need to include the code for lighting the LED array at this point in
-      // code. Look at Jorge's implementation code and add it here.
+    // These four bool variables will be used by the corresponding
+    // EMF sensor.
+    bool voltDectN;
+    bool voltDectE;
+    bool voltDectS;
+    bool voltDectW;
 
-      // The following code will determine if the extension cable is running in a straight 
-      // line or if there is a bend at this particular square.
-      legendValueSPI = cableConfig(sensorN, sensorE, sensorS, sensorW);
+    // This bool variable will be used to store whether or not there is insulation 
+    // below the layer of glass.
+    bool insulationDect;
 
-      // Send the SPI code value to the master, use the 'legendValueSPI' int variable.
+    // This variable will be used to communicate to the MCU that drives the motors.
+    int currentCableConfig = 0;
+  
+    // Use the read_Obstacle function to determine if there is an
+    // obstacle.
+    scan4Obstacle = read_Obstacle;
+
+    if(scan4Obstacle == false) {
+      voltDectN = false;
+      voltDectE = false;
+      voltDectS = false;
+      voltDectW = false;
+    
+      // This code uses all four EMF sensors to scan for a voltage.
+      sensorN.getVoltage();
+      sensorE.getVoltage();
+      sensorS.getVoltage();
+      sensorW.getVoltage();
+
+      voltDectN = sensorN.checkRange();
+      voltDectE = sensorE.checkRange();
+      voltDectS = sensorS.checkRange();
+      voltDectW = sensorW.checkRange();
+
+      // This if/statement determines whether or not any of the four EMF sensors detects a
+      // voltage. 
+      if(voltDectN == true || voltDectE == true || voltDectS == true || voltDectW == true) {
+        // I (David) need to include the code for lighting the LED array at this point in
+        // code. Look at Jorge's implementation code and add it here.
+
+        // The following code will determine if the extension cable is running in a straight 
+        // line or if there is a bend at this particular square.
+        legendValueSPI = cableConfig(sensorN, sensorE, sensorS, sensorW);
+
+        // Send the SPI code value to the master, use the 'legendValueSPI' int variable.
       
-    }
-    else {
-      //  This code will determine whether or not there is insulation present at the current
-      // location of the robot. 
-      
-
-      // This if statement will determine whether or not there is insulation present below 
-      // the glass layer.
-      if( ) {
-        
       }
-      // I (David) need to include the code for lighting the LED array at this point in
-      // code. Look at Jorge's implementation code and add it here.
-            
-    }
-  }
-  else
-  {
-    // This else statement will send a variable to the MCU that drives the motors. The purpose of 
-    // variable is to adjust the path of the robot to avoid the obstacle.
-    legendValueSPI = 9;
+      else {
+        //  This code will determine whether or not there is insulation present at the current
+        // location of the robot. 
+      
 
-    // I (David) needs to write code using SPI to communicate to the master that an obstacle has 
-    // been detected.
+        // This if statement will determine whether or not there is insulation present below 
+        // the glass layer.
+        if( ) {
+        
+        }
+        // I (David) need to include the code for lighting the LED array at this point in
+        // code. Look at Jorge's implementation code and add it here.
+            
+      }
+    }
+    else
+    {
+      // This else statement will send a variable to the MCU that drives the motors. The 
+      // purpose of variable is to adjust the path of the robot to avoid the obstacle.
+      legendValueSPI = 9;
+
+      // I (David) needs to write code using SPI to communicate to the master that an 
+      // obstacle has been detected.
+    }
   }
 }
 
@@ -181,12 +190,13 @@ bool read_Obstacle() {
 
 // This function will determine the configuration of the extension cord. There are 
 // six different configurations. Each configuration will be identified by an integer
-// variable (int values 1 - 6), look at the paper documentation for more information. This 
-// function will return the int value of the configuration.
+// variable (int values 1 - 6), look at the paper documentation for more information. 
+// This function will return the int value of the configuration.
 int cableConfig(float vN, float vE, float vS, float vW) {
-  // The 'expectedV float variable will be determined during testing of the EMF sensors. This
-  // value is an expected voltage but the EMF sensors will rarely if ever return this value. The
-  // programmer must determine a percent range of acceptable voltage values.  
+  // The 'expectedV float variable will be determined during testing of the EMF 
+  // sensors. This value is an expected voltage but the EMF sensors will rarely 
+  // if ever return this value. The programmer must determine a percent range of 
+  // acceptable voltage values.  
   const float expectedV = 5.0;
 
   const float minRangeV = 0.95 * expectV;
